c0528dbf1a3f1e00daa88a7cdb464077a2a7671d
Shivendra <tiwarishivendra589@gmail.com>
2026-05-17 22:44:02 +0530

    Fix CORS

--- database/server.js
+++ database/server.js
@@ -27,12 +27,7 @@ connectCloudinary();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175"
-    ].filter(Boolean),
+    origin: true,
     credentials: true,
   })
 );
